58d554b43131c4be8a2fedc08ef79ff3e1fcb8ad
Joel Gilley <Joel.Gilley@consilio.com>
2024-01-26 16:49:51 -0700

    Update ChatPrompt.jsx
    Added a text counter and made the button a little smarter. It should be disabled if 1. no text 2. too much text 3. currently running

--- libs/app/Investigate/chat/ChatPrompt.jsx
+++ libs/app/Investigate/chat/ChatPrompt.jsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import sageClient from "../../../utils/web/HttpClient";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -10,7 +10,10 @@ import Row from 'react-bootstrap/Row';
 export default function ChatPrompt({ query, onQuery }) {
     const text = useRef(query);
     const [loading, setLoading] = useState(false);
+    const [textLength, setTextLength] = useState(query ? query.length : 0);
+    const [canSubmitQuery, setCanSubmitQuery] = useState(false);
 
+    const maxQueyrLength = 2000;
     const placeholderText = `Ask your question here, such as "How did Enron manipulate its financial statements, and what were the consequences?"`;
 
     const onClickDelegate = (e) => {
@@ -21,16 +24,41 @@ export default function ChatPrompt({ query, onQuery }) {
             .then(() => onQuery && onQuery({ value: queryText }));
     }
 
+    const onInputDelegate = (e) => {
+        const queryText = text.current.value;
+        setTextLength(queryText.length);
+    }
+
+    useEffect(() => {
+        if (loading) {
+            console.log("loading is true: disabled");
+            setCanSubmitQuery(false);
+        }
+        else if (textLength === 0) {
+            console.log("text = 0: disabled");
+            setCanSubmitQuery(false);
+        }
+        else if (textLength > maxQueyrLength) {
+            console.log("text > max length: disabled");
+            setCanSubmitQuery(false);
+        }
+		else {
+            console.log("all good: enabled");
+            setCanSubmitQuery(true);
+        }
+	}, [ loading, textLength ]);
+
     return (
         <Form>
             <Row>
                 <Col>
                     <Form.Group>
-                        <Form.Control as="textarea" placeholder={placeholderText} rows={3} ref={text} />
+                        <Form.Control as="textarea" placeholder={placeholderText} rows={3} ref={text} onInput={onInputDelegate} />
+                        <Form.Text muted>{textLength} / {maxQueyrLength}</Form.Text>
                     </Form.Group>
                 </Col>
                 <Col xs="auto">
-                    <Button variant="secondary" onClick={onClickDelegate}>{loading ? "Loading..." : "Run"}</Button>
+                    <Button variant="secondary" onClick={onClickDelegate} disabled={!canSubmitQuery}>{loading ? "Loading..." : "Run"}</Button>
                 </Col>
             </Row>
         </Form>
