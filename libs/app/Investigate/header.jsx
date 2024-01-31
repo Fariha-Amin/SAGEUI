fc346b71458a977e2f6d71bf552616d08a46c9a6
Joel Gilley <Joel.Gilley@consilio.com>
2024-01-31 15:11:31 -0700

    RPMXCON-83839 Aligned text
    Also added link to FAQ site

--- libs/app/Investigate/header.jsx
+++ libs/app/Investigate/header.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from 'react';
 import { Col, Navbar, Button, Row, FormLabel } from 'react-bootstrap';
+import Links from "../../shared/links";
 import DocCounter from '../../shared/doc-counter/DocCounter';
 import IconButton from '../../shared/icon-button/IconButton';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,24 +11,31 @@ const Bold = styled.div`
 `;
 
 const Header = () => (
-    <Navbar>
-        <Col>
+    <>
+        <Row>
             <Col>
-                <FormLabel><Bold>neXgenAI Investigate </Bold></FormLabel>
-                <IconButton icon="circle-question" />
+                <Col>
+                    <FormLabel><Bold>neXgenAI Investigate </Bold></FormLabel>
+                    <IconButton icon="circle-question" />
+                </Col>
             </Col>
-        </Col>
-        <Col>
-            <Row>
-                <Col>
-                    <DocCounter label='Total Documents '/>
-                </Col>
-                <Col>
-                    <Button>Manage Document Population</Button>
-                </Col>
-            </Row>
-        </Col>
-    </Navbar>
+            <Col>
+                <Row>
+                    <Col>
+                        <DocCounter label='Total Documents ' />
+                    </Col>
+                    <Col>
+                        <Button>Manage Document Population</Button>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+        <Row>
+            <p>
+                Enter a question to start your investigation process. For guidance on using the neXgenAI features in Sightline, please refer to the <a href={Links.GenAiFaq}>FAQ</a>.
+            </p>
+        </Row>
+    </>
 )
 
 export default Header;
