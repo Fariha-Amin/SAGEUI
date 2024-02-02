5dde3cd2b7df1529ac6d2064b51031dcc88ae1c1
Parvez.Mahmud <parvez.mahmud@consilio.com>
2024-02-02 17:38:04 +0600

    Summarize Skeleton added.

---
+++ app/Summarize/app.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export default function App(){
+
+    return <h1>Welcome to Sage UI summarize</h1>
+    
+}
---
+++ summarize.webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  entry: './app/Summarize/index.js',
+  resolve: {extensions: ['*','.js','.jsx']},
+  output: {
+    path:path.resolve(__dirname, "dist/SAGEUI/Summarizer"),
+    filename: '[contenthash].js',
+    publicPath: '/',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {presets: ['@babel/preset-env', '@babel/preset-react']}
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ]
+  },
+  devServer:{
+        static:{
+            directory: path.join(__dirname, 'dist/Summarizer/')
+        },
+        port:3001,
+        devMiddleware:{
+            publicPath: 'http://localhost:3001'
+        }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({title: "Summarizer", template: path.join(__dirname, "index.html"),}),
+    new webpack.HotModuleReplacementPlugin()
+  ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
+  optimization: {
+    minimize:true,
+    minimizer: [
+        new TerserPlugin({
+            extractComments: false
+        })
+    ]
+  }
+}
