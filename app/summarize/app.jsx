5eda478b69f27b2aa9247cab4eac5ea53ed50fe1
Parvez.Mahmud <parvez.mahmud@consilio.com>
2024-02-09 18:47:06 +0600

    Basic Theme imported from LegalHold

--- app/summarize/app.jsx
+++ app/summarize/app.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import SummaryHeader from "./modules/summary/SummaryHeader";
+
 import SageDataTable from './components/SageDataTable';
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import './App.css';
 
