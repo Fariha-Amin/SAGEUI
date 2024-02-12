cf3e4390d37fe682d5ad2a4e75b5e59bd60c5750
md-imran97 <mohammed.imran@consilio.com>
2024-02-12 20:36:06 +0600

    Update SageDataTable.jsx

--- libs/shared/data-grid/SageDataTable.jsx
+++ libs/shared/data-grid/SageDataTable.jsx
@@ -16,7 +16,8 @@ function createColumnDefination(columnDef, skeleton = false){
             <Column body={<Skeleton />} field="User" header="User" sortable filter showFilterMenu={false}/>,
             <Column body={<Skeleton />} field="DocumentId" header="DocId (Fed to AI)" sortable filter showFilterMenu={false}/>,
             <Column body={<Skeleton />} field="Summary" header="Summary" sortable filter showFilterMenu={false}/>,
-            <Column body={<Skeleton />} field="Notes" header="Notes" sortable filter showFilterMenu={false}/>
+            <Column body={<Skeleton />} field="Notes" header="Notes" sortable filter showFilterMenu={false}/>,
+            <Column body={<Skeleton />}  showFilterMenu={false} headerStyle={{ width: '10rem' }} />
         ];
     }else{
         return [
@@ -25,7 +26,8 @@ function createColumnDefination(columnDef, skeleton = false){
             <Column field="User" header="User" sortable filter showFilterMenu={false} showClearButton={false} align="center" />,
             <Column field="DocumentId" header="DocId (Fed to AI)" sortable filter showFilterMenu={false} showClearButton={false} align="center"/>,
             <Column field="Summary" header="Summary" body={renderSummary}  filter showFilterMenu={false} showClearButton={false} align="center" />,
-            <Column field="Notes" header="Notes" sortable filter showFilterMenu={false} showClearButton={false} align="center" />
+            <Column field="Notes" header="Notes" sortable filter showFilterMenu={false} showClearButton={false} align="center" />,
+            <Column showFilterMenu={false} headerStyle={{ width: '10rem' }} />
         ]
     }
     // columnDef.forEach(column => {
@@ -38,13 +40,35 @@ function createColumnDefination(columnDef, skeleton = false){
 
 
 export default function SageDataTable(props) {
-    const {showSkeleton} = props;
 
     const [summmaryData, setSummmaryData] = useState([]);
     const [columnDefinations, setColumnDefinations] = useState([]);
+    const [expandedRows, setExpandedRows] = useState(null);
 
     const {tableConfig} = props;
     const {columnDef} = props;
+
+    // Expanding row logic
+    const onRowClick = (e)=>{
+        let data = e.data;
+        let newExpandData = null;
+        if(expandedRows == null){
+            newExpandData = [data];
+        }else{
+            let filterData = expandedRows.filter(d=> d.RecId != data.RecId);
+            if(filterData.length == expandedRows.length){
+                filterData.push(data); 
+            }
+            newExpandData = filterData.length ? [...filterData] : null;
+        }
+        setExpandedRows(newExpandData);
+    }
+    // Expanding row template
+    const rowExpansionTemplate = (data)=>{
+        return(
+            <td colSpan={6}>{data.Summary}</td>
+        );
+    }
 
     const defaultTableConfig = {
         value:summmaryData,
@@ -58,8 +82,11 @@ export default function SageDataTable(props) {
         cellSelection:true,
         paginatorLeft:true,
         tableClassName:"table table-striped table-hover table-bordered align-middle dataTable no-footer",
-        paginatorTemplate:"CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink  RowsPerPageDropdown",
-        currentPageReportTemplate:"Total: {totalRecords} entries"
+        paginatorTemplate:"CurrentPageReport PrevPageLink PageLinks NextPageLink RowsPerPageDropdown",
+        currentPageReportTemplate:"Total: {totalRecords} entries",
+        onRowClick:onRowClick,
+        expandedRows:expandedRows,
+        rowExpansionTemplate:rowExpansionTemplate
     };
 
     
@@ -76,7 +103,6 @@ export default function SageDataTable(props) {
         ProductService.getSummaryData().then(data => setSummmaryData(data));
         setTimeout(loadData, 3000);
     }, []);
-
     
     return(
         <DataTable {...{...defaultTableConfig, ...tableConfig}} >
