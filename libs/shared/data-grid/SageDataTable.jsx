1b877d7bcc1e51c0067c3302b521a5a529283f02
md-imran97 <mohammed.imran@consilio.com>
2024-02-15 20:47:58 +0600

    Update column definition and table config

--- libs/shared/data-grid/SageDataTable.jsx
+++ libs/shared/data-grid/SageDataTable.jsx
@@ -1,18 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { ProductService } from '../../../app/summarize/service/ProductService';
-import util from './utility/sageTableUtility'
+import sageTableUtil from './utility/sageTableUtility'
 
 export default function SageDataTable(props) {
 
-    const { tableConfig } = props;
-    const { columnDef } = props;
+    const tableConfig = sageTableUtil.createTableConfig(props);
+    const columnDef  = props.children;
 
     const [summmaryData, setSummmaryData] = useState([]);
-    const [columnDefinations, setColumnDefinations] = useState(util.createColumnDefinition(columnDef, false));
+    const [columnDefinations, setColumnDefinations] = useState(sageTableUtil.createColumnDefinition(columnDef, false));
     const [expandedRows, setExpandedRows] = useState(null);
-
-
 
     // Expanding row logic
     const onCellClick = (e) => {
@@ -36,43 +34,27 @@ export default function SageDataTable(props) {
         );
     }
 
-    const defaultTableConfig = {
-        value: summmaryData,
-        dataKey:"RecId",
-        resizableColumns: true,
-        showGridlines: true,
-        stripedRows: true,
-        paginator: true,
-        rows: 25,
-        filterDisplay: "row",
-        tableStyle: { minWidth: '50rem' },
-        cellSelection: true,
-        paginatorLeft: true,
-        tableClassName: "table table-border table-hover table-bordered align-middle dataTable no-footer",
-        paginatorTemplate: "CurrentPageReport PrevPageLink PageLinks NextPageLink RowsPerPageDropdown",
-        currentPageReportTemplate: "Total: {totalRecords} entries",
-        onCellClick: onCellClick,
-        expandedRows: expandedRows,
-        rowExpansionTemplate: rowExpansionTemplate
-    };
-
-
-
     const loadData = () => {
         ProductService.getSummaryData().then(data => {
             setSummmaryData(data)
-            setColumnDefinations(util.createColumnDefinition(columnDef, false));
+            setColumnDefinations(sageTableUtil.createColumnDefinition(columnDef, false));
         });
     }
 
     useEffect(() => {
-        setColumnDefinations(util.createColumnDefinition(columnDef, true));
+        setColumnDefinations(sageTableUtil.createColumnDefinition(columnDef, true));
         ProductService.getSummaryData().then(data => setSummmaryData(data));
         setTimeout(loadData, 1000);
     }, []);
 
     return (
-        <DataTable {...{ ...defaultTableConfig, ...tableConfig }} >
+        <DataTable 
+        {...tableConfig}
+        value={summmaryData}
+        onCellClick={onCellClick} 
+        expandedRows={expandedRows}
+        rowExpansionTemplate={rowExpansionTemplate}
+        >
             {columnDefinations}
         </DataTable>
     );
