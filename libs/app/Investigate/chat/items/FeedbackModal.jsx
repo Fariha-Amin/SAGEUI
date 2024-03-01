c52d3da9cf95f8d655712915fc3596c9a36c7912
Tylor Emmett <Tylor.Emmett@consilio.com>
2024-03-01 09:58:27 -0500

    RPMXCON-83745
    Completed styling

--- libs/app/Investigate/chat/items/FeedbackModal.jsx
+++ libs/app/Investigate/chat/items/FeedbackModal.jsx
@@ -1,25 +1,54 @@
-import './Item.scss';
+import './FeedbackModal.scss';
 import React from 'react';
-import { useState } from "react";
-import { Accordion, AccordionTab } from 'primereact/accordion';
+import { useState, useRef } from "react";
 import { Dialog } from 'primereact/dialog';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { Button } from 'primereact/button';
 import IconButton from '_shared/icon-button/IconButton';
-import Actions from './Actions';
-import Answer from './Answer';
-import Question from './Question';
 import sageClient from "_investigate/httpClient";
 
 
+
+
 const FeedbackModal = (props) => {
+    const [textLength, setTextLength] = useState(0);
+    const text = useRef("");
+    const helpText = "If you are not satisfied with the response, please let us know. For more optimal results, please be specific when forming your questions. The default investigation prompt is a guide on how to format questions to the LLM.";
+    const maxTextLength = 400;
+    const placeholderText = `Enter your feedback for this question & answer here.`;
+    const textCounter = <small className='feedback-modal__text-counter'>{textLength} / {maxTextLength}</small>
+    const maxTextError = <small className='feedback-modal__max-text-error'>You have exceeded the max character limit.</small>
+    const headerContent = <div className="flex align-items-center">
+                                <b>Report Bad Response</b>
+                                <IconButton className="sage-icon-superscript" icon="circle-question" title={helpText} titlePlacement="bottom" />
+                        </div>
+
+    var counterContent = textCounter;
+
+    const onInputDelegate = (e) => {
+        const currentText = text.current.value;
+        setTextLength(currentText.length);
+    }
+
     return(
-        <Dialog header="Report Bad Response" visible={props.shouldShow} style={{ width: '50vw', height: '50vh' }} onHide={() => props.onClose(false)}>
-            <InputTextarea rows={2} style={{ width: '46vw', height: '25vh'}}/>
+        <Dialog header={headerContent} visible={props.shouldShow} className='feedback-modal__dialog' onHide={() => props.onClose(false)}>
+            <InputTextarea 
+                maxLength={maxTextLength}
+                className='feedback-modal__text-area'
+                placeholder={placeholderText}
+                ref={text}
+                onInput={onInputDelegate}
+                rows={2} 
+            />
             <div>
-                <Button label="Cancel" raised outlined size="small" severity="secondary" onClick={() => props.onClose(false) } />
+                {textLength === maxTextLength ? maxTextError : textCounter}
+            </div>
+            <div className='feedback-modal__buttons'>
+                <Button label="Save" raised size="small" />
                 &nbsp;
-                <Button label="Save" raised size="small" severity="info" />
+                <Button label="Cancel" raised outlined size="small" severity="secondary" onClick={() => props.onClose(false) } />
+                
+                
             </div>
         </Dialog>
     );
