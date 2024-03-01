b021209570b6067fd96d48c5e0b16281e6cd90c0
Parvez.Mahmud <parvez.mahmud@consilio.com>
2024-03-01 16:12:14 +0600

    Prime react theme added.

--- app/summarize/index.js
+++ app/summarize/index.js
@@ -4,6 +4,7 @@ import App from "./app";
 import { PrimeReactProvider } from "primereact/api";
 import "primeflex/primeflex.css";
 import "primereact/resources/primereact.css";
+import 'primereact/resources/themes/lara-light-indigo/theme.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
