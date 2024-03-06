c53dc13819c2415ca4ba978d338dbde29f40e1bc
Joel Gilley <Joel.Gilley@consilio.com>
2024-03-06 08:57:39 -0700

    More tests

--- libs/shared/data-table/DataTable.jsx
+++ libs/shared/data-table/DataTable.jsx
@@ -1,9 +1,24 @@
 import './DataTable.scss'
 import React from "react";
+import { useState } from 'react';
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 
+function getDefaultSort(props) {
+    // Pull sort information off the columns like so:
+    // <DataTable>
+    //   <DataColumn field="foo" sortable sortOrder={1}/>
+    //   <DataColumn field="bar" sortable sortOrder={-1}/>
+    // </DataTable>
+    return props.children
+        .map((column, i) => { return { field: column.props.field, order: column.props.sortOrder }; })
+        .filter((sort, i) => sort.field && sort.order);
+};
+
 export default function SageDataTable(props) {
+    const [sortField, setSortField] = useState(null);
+    const [sortOrder, setSortOrder] = useState(null);
+    const [multiSortMeta, setMultiSortMeta] = useState(getDefaultSort(props));
 
     let tableProps = {};
 
@@ -23,6 +38,15 @@ export default function SageDataTable(props) {
     tableProps.reorderableRows = false;
     tableProps.scrollable = true;
     tableProps.scrollHeight = "flex";
+    if (props.scrollHeight) {
+        // Customize scrolling e.g. <DataTable scrollHeight="400px">
+        tableProps.scrollHeight = props.scrollHeight;
+    }
+    tableProps.emptyMessage = (
+        <div style={{ textAlign: "center" }}>
+            Your query returned no data
+        </div>
+    );
 
     // Apply paging if requested
     // Example: <DataTable rowsPerPage={25} rowsPerPageOptions={[25,50,100]} onPage={(e) => setPage(e.first, e.rows, e.page, e.pageCount)}>
@@ -36,7 +60,7 @@ export default function SageDataTable(props) {
 
     // Apply sorting if any column is "sortable=true"
     // Example: 
-    // <DataTable onSort={(e) => setSort(e)}>
+    // <DataTable onSort={(e) => setSort(e.first, e.rows, e.filters, e.sort)}>
     //   <DataColumn sortable sortOrder={1}/>
     // </DataTable>
     const sortingEnabled = props.children.filter((column, i) => column.props.sortable === true);
@@ -47,11 +71,24 @@ export default function SageDataTable(props) {
         //  1 - ascending
         tableProps.sortMode = "multiple";
         tableProps.removableSort = true;
-        const fieldOrder = props.children
-            .map((column, i) => { return { field: column.props.field, order: column.props.sortOrder }; })
-            .filter((sort, i) => sort.field && sort.order);
-        tableProps.multiSortMeta = fieldOrder;
-        tableProps.onSort = props.onSort;
+        tableProps.multiSortMeta = multiSortMeta;
+        tableProps.sortField = sortField;
+        tableProps.sortOrder = sortOrder;
+
+        function onSortDelegate(e) {
+            setSortField(e.sortField);
+            setSortOrder(e.sortOrder);
+            setMultiSortMeta(e.multiSortMeta);
+
+            const modifiedEvent = {
+                first: e.first,
+                rows: e.rows,
+                filters: e.filters,
+                sort: e.multiSortMeta
+            };
+            props.onSort && props.onSort(modifiedEvent);
+        };
+        tableProps.onSort = onSortDelegate;
     }
 
     // Apply row selection if requested
@@ -71,12 +108,6 @@ export default function SageDataTable(props) {
         }
     }
 
-    // Customize scrolling
-    // Example: <DataTable scrollHeight="400px">
-    if (props.scrollHeight) {
-        tableProps.scrollHeight = props.scrollHeight;
-    }
-
     // Apply state - sort, page, etc. will be remembered across page reloads
     // Example: <DataTable id={uniqueId} stateful>
     const stateEnabled = props.stateful ? true : false;
@@ -88,14 +119,8 @@ export default function SageDataTable(props) {
         }
     }
 
-    const noDataMessage = (
-        <div style={{ textAlign: "center" }}>
-            Your query returned no data
-        </div>
-    );
-
     return (
-        <DataTable {...tableProps} emptyMessage={noDataMessage}>
+        <DataTable {...tableProps}>
             {selectColumn}
             {props.children}
         </DataTable>
--- libs/shared/data-table/DataTable.test.js
+++ libs/shared/data-table/DataTable.test.js
@@ -100,6 +100,7 @@ describe("DataTable UX", () => {
                     <DataColumn headerClassName="column3" field="field3" sortable />
                 </DataTable>
             ));
+
             const th = document.querySelector(".column1");
             await userEvent.click(th);
 
@@ -125,11 +126,149 @@ describe("DataTable UX", () => {
                     <DataColumn headerClassName="column3" field="field3" />
                 </DataTable>
             ));
+
             const th = document.querySelector(".column1");
             await userEvent.click(th);
 
             // Assert
             expect(handleOnSort).not.toHaveBeenCalled();
+        });
+
+        test('sort ascending - default order - works as expected', async () => {
+            // Arrange
+            const handleOnSort = jest.fn();
+            const testData = [
+                { field1: 1, field2: "a", field3: "I" },
+                { field1: 2, field2: "b", field3: "II" },
+                { field1: 3, field2: "c", field3: "III" }
+            ];
+
+            // Act
+            render((
+                <DataTable id="testTable" value={testData} onSort={handleOnSort}>
+                    <DataColumn headerClassName="column1" field="field1" sortable sortOrder={1} />
+                    <DataColumn headerClassName="column2" field="field2" />
+                    <DataColumn headerClassName="column3" field="field3" />
+                </DataTable>
+            ));
+
+            const rows = document.querySelectorAll("tr");
+            const row1 = rows[1];
+            const row1column1 = row1.querySelectorAll("td")[0];
+            const row2 = rows[2];
+            const row2column1 = row2.querySelectorAll("td")[0];
+            const row3 = rows[3];
+            const row3column1 = row3.querySelectorAll("td")[0];
+
+            // Assert
+            expect(row1column1.innerHTML).toBe("1");
+            expect(row2column1.innerHTML).toBe("2");
+            expect(row3column1.innerHTML).toBe("3");
+        });
+
+        test('sort ascending - on click - works as expected', async () => {
+            // Arrange
+            const handleOnSort = jest.fn();
+            const testData = [
+                { field1: 1, field2: "a", field3: "I" },
+                { field1: 2, field2: "b", field3: "II" },
+                { field1: 3, field2: "c", field3: "III" }
+            ];
+
+            // Act
+            render((
+                <DataTable id="testTable" value={testData} onSort={handleOnSort}>
+                    <DataColumn headerClassName="column1" field="field1" sortable sortOrder={0} />
+                    <DataColumn headerClassName="column2" field="field2" />
+                    <DataColumn headerClassName="column3" field="field3" />
+                </DataTable>
+            ));
+
+            // Click the column ONCE to sort ASCENDING
+            const th = document.querySelector(".column1");
+            await userEvent.click(th);
+
+            const rows = document.querySelectorAll("tr");
+            const row1 = rows[1];
+            const row1column1 = row1.querySelectorAll("td")[0];
+            const row2 = rows[2];
+            const row2column1 = row2.querySelectorAll("td")[0];
+            const row3 = rows[3];
+            const row3column1 = row3.querySelectorAll("td")[0];
+
+            // Assert
+            expect(row1column1.innerHTML).toBe("1");
+            expect(row2column1.innerHTML).toBe("2");
+            expect(row3column1.innerHTML).toBe("3");
+        });
+
+        test('sort descending - default order - works as expected', async () => {
+            // Arrange
+            const handleOnSort = jest.fn();
+            const testData = [
+                { field1: 1, field2: "a", field3: "I" },
+                { field1: 2, field2: "b", field3: "II" },
+                { field1: 3, field2: "c", field3: "III" }
+            ];
+
+            // Act
+            render((
+                <DataTable id="testTable" value={testData} onSort={handleOnSort}>
+                    <DataColumn headerClassName="column1" field="field1" sortable sortOrder={-1} />
+                    <DataColumn headerClassName="column2" field="field2" />
+                    <DataColumn headerClassName="column3" field="field3" />
+                </DataTable>
+            ));
+            
+            const rows = document.querySelectorAll("tr");
+            const row1 = rows[1];
+            const row1column1 = row1.querySelectorAll("td")[0];
+            const row2 = rows[2];
+            const row2column1 = row2.querySelectorAll("td")[0];
+            const row3 = rows[3];
+            const row3column1 = row3.querySelectorAll("td")[0];
+
+            // Assert
+            expect(row1column1.innerHTML).toBe("3");
+            expect(row2column1.innerHTML).toBe("2");
+            expect(row3column1.innerHTML).toBe("1");
+        });
+
+        test('sort descending - on click - works as expected', async () => {
+            // Arrange
+            const handleOnSort = jest.fn();
+            const testData = [
+                { field1: 1, field2: "a", field3: "I" },
+                { field1: 2, field2: "b", field3: "II" },
+                { field1: 3, field2: "c", field3: "III" }
+            ];
+
+            // Act
+            render((
+                <DataTable id="testTable" value={testData} onSort={handleOnSort}>
+                    <DataColumn headerClassName="column1" field="field1" sortable sortOrder={0} />
+                    <DataColumn headerClassName="column2" field="field2" />
+                    <DataColumn headerClassName="column3" field="field3" />
+                </DataTable>
+            ));
+
+            // Click the column TWICE to sort DESCENDING
+            const th = document.querySelector(".column1");
+            await userEvent.click(th);
+            await userEvent.click(th);
+
+            const rows = document.querySelectorAll("tr");
+            const row1 = rows[1];
+            const row1column1 = row1.querySelectorAll("td")[0];
+            const row2 = rows[2];
+            const row2column1 = row2.querySelectorAll("td")[0];
+            const row3 = rows[3];
+            const row3column1 = row3.querySelectorAll("td")[0];
+
+            // Assert
+            expect(row1column1.innerHTML).toBe("3");
+            expect(row2column1.innerHTML).toBe("2");
+            expect(row3column1.innerHTML).toBe("1");
         });
     });
 });
