43a056f487220f1bc87ea021414ccafb582421bc
Afranul Haque <afran.mridha@gmail.com>
2024-03-06 17:07:09 +0600

    RPMXCON-85110/feat: added style for button and text

--- app/summarize/app.jsx
+++ app/summarize/app.jsx
@@ -32,7 +32,7 @@ export default function App() {
   };
 
   return (
-    <div className="container-fluid" style={{ padding: "2rem 2rem" }}>
+    <div className="container-fluid" style={{ padding: "4rem 4rem 0rem 0rem" }}>
       <SummaryHeader />
 
       <SageDataTable
