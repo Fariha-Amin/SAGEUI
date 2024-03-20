5b0e47b59ab4cdcb78335662940e4bc643909f47
Anwar Pattathil <Anwar.Pattathil@Consilio.com>
2024-03-20 11:07:28 -0500

    Implemented review changes

--- app/summarize/views/createNewSummary/components/SelectSummaryTypeTab.jsx
+++ app/summarize/views/createNewSummary/components/SelectSummaryTypeTab.jsx
@@ -1,43 +1,48 @@
-import React,{useState} from "react";
-import RadioSelector from "../../../components/RadioSelector";
+import React, { useState } from "react";
 import HelpIconSm from "../../../icons/help_18.svg";
 import LabelWithHelpIcon from "../../../components/LabelWithHelpIcon";
+import RadioCategorySelector from "./RadioCategorySelector";
 
 function SelectSummaryTypeTab() {
   const summaryTypes = [
     { name: "An individual summary for each document", key: 1 },
-    { name: "A combined summary for a set of documents (Max of 25 documents)", key: 2 },
+    {
+      name: "A combined summary for a set of documents (Max of 25 documents)",
+      key: 2,
+    },
   ];
   const [selectedSummaryType, setSelectedSummaryType] = useState(1);
   return (
     <>
-
-    <div className="row mt-4">
-      <div className="col-8">
-        <div className="col">
-          <div className="position-relative">
-          <LabelWithHelpIcon
-            label={"Select Your Summary Type"}
-            icon={HelpIconSm}
-          />
+      <div className="row mt-4">
+        <div className="col-8">
+          <div className="col">
+            <LabelWithHelpIcon
+              label={"Select Your Summary Type"}
+              icon={HelpIconSm}
+            />
+          </div>
+          <div className="col">
+            <RadioCategorySelector
+              categories={summaryTypes}
+              selectedCategory={selectedSummaryType}
+              onRadioSelectionChange={setSelectedSummaryType}
+              categoryContainer="d-flex align-items-center mb-3"
+            />
+          </div>
+          <div>
+            <label className="summary-document-category-label font-style-italics ml-6">
+              Note: When selecting a combined summary, a maximum of 25 documents
+              will be sent{" "}
+              <p>
+                to the AI engine for summarization. Anything exceeding this
+                limit will be excluded.
+              </p>
+            </label>
           </div>
         </div>
-        <div className="col">
-          <RadioSelector
-            options={summaryTypes}
-            selectedOption={selectedSummaryType}
-            setSelectedOption={setSelectedSummaryType}
-          />
-        </div>
-        <div>
-           <label className="summary-document-category-label font-style-italics ml-6">
-               Note: When selecting a combined summary, a maximum of 25 documents will be sent <p>to the AI engine for summarization. Anything exceeding this limit will be excluded.</p> 
-           </label>
-        </div>
       </div>
-    </div>
-  </>
-
+    </>
   );
 }
 
